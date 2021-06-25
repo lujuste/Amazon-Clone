1b661099f5dc4cd29e6928d0093fe4463abae513
Lucas Juste <lu.scatollini@hotmail.com>
2021-06-25 15:36:25 -0300

    first commit

---
+++ my-amazo/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="app">
+        <Header />
+        <Home />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-amazo/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLS2hCCS7--RcPARjWpwDTMRtxvHmVevs",
+    authDomain: "clone-cb892.firebaseapp.com",
+    projectId: "clone-cb892",
+    storageBucket: "clone-cb892.appspot.com",
+    messagingSenderId: "413165720591",
+    appId: "1:413165720591:web:59c68240fb6af33f34bf7a",
+    measurementId: "G-1Z6MJELN9V"
+  };
